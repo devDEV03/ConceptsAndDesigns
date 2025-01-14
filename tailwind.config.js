19f7b987975fde4898f835f09ea18d431cd8893a
Dev Singhal <singhaldev26@gmail.com>
2025-01-14 12:36:38 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    content: ["./*.html", "./css/**/*.css"], // Specify paths to your HTML and CSS files
+    theme: {
+      extend: {
+        colors: {
+          customColor: '#ff5722',
+        },
+        fontFamily: {
+          serif: ['Space Grotesk', 'serif'], // Add your custom font here
+        },
+      },
+    },
+    plugins: [],
+  };
+  
